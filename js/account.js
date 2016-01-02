1d1eb5df37d6e3dd5d194ed32075afa32bfde3a7
Peter Stromhaug <pas282@cornell.edu>
2016-01-02 00:16:13 -0500

    signin functinality added back in

---
+++ js/account.js
@@ -0,0 +1,11 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+
+    $('#profimg').attr('src', profile.getImageUrl());
+    $('#signinbtn').css('display','none');
+    $('#namefield').text(profile.getName()).css('display','inline');
+}
